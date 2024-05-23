a6cda4f127f10b2e807482754f5606a2de0f08c4
faresfares15 <lf_aoufar@esi.dz>
2024-05-23 11:35:22 +0100

    started the login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,13 +2,18 @@ import { login, signup } from './actions'
 
 export default function LoginPage() {
     return (
+        <main className={"flex flex-col justify-center items-center h-screen w-screen"}>
+            <h1 className={"text-6xl font-bold"}>Sign Up</h1>
         <form>
-            <label htmlFor="email">Email:</label>
-            <input id="email" name="email" type="email" required />
+            <div className={"flex flex-col"}>
+                <label htmlFor="email">Email:</label>
+                <input id="email" name="email" type="email" required className={"bg-[#F2F1EE] opacity-60 px-3 py-2 rounded-lg shadow-2xl focus:shadow-[#2383E2] focus:drop-shadow-sm"}/>
+            </div>
             <label htmlFor="password">Password:</label>
             <input id="password" name="password" type="password" required />
             <button formAction={login}>Log in</button>
             <button formAction={signup}>Sign up</button>
         </form>
+        </main>
     )
 }
